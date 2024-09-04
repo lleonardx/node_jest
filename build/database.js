874941dcaca188b23f9d79a958dc0f4161a02e94
lleonardx <lleonard.alves@gmail.com>
2024-09-04 15:52:42 -0400

    feat: implements new functions

---
+++ build/database.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.database = void 0;
+const database = [
+    "Leonardo",
+    "Jonas",
+    "Heloyse",
+    "Vinicius"
+];
+exports.database = database;
